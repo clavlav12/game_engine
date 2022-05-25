5da840c503968c8c41c33ad3830b33d3f38154ad
clavlav12 <avi.ros.2003@gmail.com>
2022-05-25 18:57:07 +0300

    Removed irrelevant files

--- Engine/NetworkingNewBeta/Server Tester.py
+++
@@ -1,9 +0,0 @@
-from server import CommandServer
-
-
-class MyServer(CommandServer):
-
-    @CommandServer.command
-    def add(self, user, kwargs):
-        self.send_message(user, 'result', result=int(kwargs['a']) + int(kwargs['b']))
-
